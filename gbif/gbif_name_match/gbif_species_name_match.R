904df9511190b897269fcbfd61cba17f6acf68a0
stijnvanhoey <stijnvanhoey@gmail.com>
2017-09-18 18:16:39 +0200

    Add function for matching entire dataframe

---
+++ gbif/gbif_name_match/gbif_species_name_match.R
@@ -0,0 +1,43 @@
+
+library('rgbif')
+library('dplyr')
+library('assertthat')
+library('assertable')
+
+
+#' Add species information provided by the GBIF taxonomic backbone API
+#'
+#' This functions extends an existing dataframe with additional columns provided
+#' by the GBIF taxonomic backbone and matched on the species scientific name,
+#' which need to be an available column in the dataframe.
+#'
+#' This function is essentially a wrapper around the existing rgbif
+#' functionality `name_lookup()` and extends the application to a data.frame.
+#' For more information on the name matching API of GBIF on which rgbif relies,
+#' see https://www.gbif.org/developer/species
+#'
+#' @param df data.frame with species information
+#' @param name_col char column name of the column containing the scientific
+#' names used for the name matching with the GBIF taxonomic backbone.
+#' @param list list of valid GBIF terms
+#'
+#'@return df with GBIF information as additional columns
+#'
+request_species_information <- function(df, gbif_terms = c('usageKey',
+                                                           'scientificName',
+                                                           'rank',
+                                                           'status',
+                                                           'family')) {
+
+    # test incoming arguments
+    assert_that(is.data.frame(df))
+    assert_colnames(df, "scientificName", only_colnames = FALSE)
+
+    # matching the GBiF matching information to the sample_data
+    df %>% rowwise() %>%
+        do(as.data.frame(name_backbone(name = .$scientificName))) %>%
+        select(gbif_terms) %>%
+        bind_cols(df)
+}
+
+
