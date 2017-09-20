cdb7e2394c7adc7e911931e931c7335205578b9a
Thierry Onkelinx <ThierryO@users.noreply.github.com>
2017-09-20 09:38:05 +0200

    correct comment on NSE
    note that Standard evaluation is required when using the function in a package. Only using `do_()` and `select_()` in function is better practice.

--- gbif/gbif_name_match/gbif_species_name_match.R
+++ gbif/gbif_name_match/gbif_species_name_match.R
@@ -51,7 +51,9 @@ request_species_information <- function(df, name_col,
         do(as.data.frame(name_backbone(name = .[[name_col]]))) %>%
         select(gbif_terms) %>%
         bind_cols(df)
-    # (remark I use here Standard evaluation (SE) do instead of the NSE do_,
+    # (remark I use here Non Standard evaluation (SE) do instead of the SE do_,
     # see also:
+    # https://cran.r-project.org/web/packages/dplyr/vignettes/programming.html and
     # https://stackoverflow.com/questions/26739054/using-variable-column-names-in-dplyr-do)
+
 }
