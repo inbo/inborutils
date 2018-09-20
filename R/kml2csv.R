875c72adf6ce932292cf214726a37505b88f6ed9
stijnvanhoey <stijnvanhoey@gmail.com>
2018-09-20 19:46:07 +0200

    Refactor to date as colname

--- R/kml2csv.R
+++ R/kml2csv.R
@@ -1,9 +1,9 @@
-
 #' Extract date and coordinate information from Google Maps kml file
 #'
 #' @param filename (char) filename/path of the kml file
 #'
-#' @return df with columns datum
+#' @return df with columns date (as chars), hour (as char), x (numeric),
+#'     y (numeric)
 #' @export
 #'
 #' @importFrom tidyr separate
@@ -19,18 +19,18 @@ read_kml_file <- function(filename) {
         separate(ruw, c("x", "y", "z"), ",")
 
     dates <- grep("<name>", kml.text)
-    date <- as.data.frame(kml.text[dates])
-    date <- date[-1,]
-    date <- as.data.frame(date)
-    colnames(date) <- c("ruw")
-    datum <- as.data.frame(substr(date$ruw, 13,22))
-    colnames(datum) <- c("datum")
-    time <- extract(date, c("ruw"), into = c("hours", "minutes"),
+    dates <- as.data.frame(kml.text[dates])
+    dates <- dates[-1,]
+    dates <- as.data.frame(dates)
+    colnames(dates) <- c("ruw")
+    date <- as.data.frame(substr(dates$ruw, 13, 22))
+    colnames(date) <- c("date")
+    time <- extract(dates, c("ruw"), into = c("hours", "minutes"),
                     "([[:digit:]]+):([[:digit:]]+)")
     time$time <- paste(time$hours, time$minutes, sep = ":")
     hour <- time[,3]
 
-    data <- cbind(datum, hour, coords[,1:2])
+    data <- cbind(date, hour, coords[,1:2])
     data$x <- as.numeric(data$x)
     data$y <- as.numeric(data$y)
     return(data)
