886c9f5271d04ccd46cff6c10e1b605bbf5ab80b
stijnvanhoey <stijnvanhoey@gmail.com>
2018-09-20 17:36:08 +0200

    Exclude file handling from function setup

--- R/kml2csv.R
+++ R/kml2csv.R
@@ -1,33 +1,30 @@
 library(tidyr)
 
-setwd("../data/kml_example_data")
-fileNames <- Sys.glob("*.kml")
+kml2csv <- function(filename) {
+    kml.text <- readLines(filename)
+    coords <- grep("<coordinates>", kml.text)
+    coord <- as.data.frame(kml.text[coords + 1])
+    colnames(coord) <- c("ruw")
+    coords <- coord %>%
+        separate(ruw, c("x", "y", "z"), ",")
 
-for (fileName in fileNames) {
-  kml.text <- readLines(fileName)
-  coords <- grep("<coordinates>",kml.text)
-  coord <- as.data.frame(kml.text[coords+1])
-  colnames(coord) <- c("ruw")
-  coords <- coord %>%
-    separate(ruw, c("x", "y", "z"), ",")
-  
-  dates <- grep("<name>",kml.text)
-  date <- as.data.frame(kml.text[dates])
-  date <- date[-1,]
-  date <- as.data.frame(date)
-  colnames(date) <- c("ruw")
-  datum <- as.data.frame(substr(date$ruw, 13,22))
-  colnames(datum) <- c("datum")
-  time <- extract(date, c("ruw"), into=c("hours","minutes"), "([[:digit:]]+):([[:digit:]]+)")
-  time$time <- paste(time$hours,time$minutes, sep=":")
-  uur<- time[,3]
-  
-  Gegevens <- cbind(datum,uur,coords[,1:2])
-  Gegevens$x <- as.numeric(Gegevens$x)
-  Gegevens$y <- as.numeric(Gegevens$y)
-  fileName <- substr(fileName,1,6)
-  Gegevens$bever <- fileName
-  write.csv2(Gegevens, file=sprintf('../csv_example_data/%s.csv', fileName), row.names = F)
+    dates <- grep("<name>",kml.text)
+    date <- as.data.frame(kml.text[dates])
+    date <- date[-1,]
+    date <- as.data.frame(date)
+    colnames(date) <- c("ruw")
+    datum <- as.data.frame(substr(date$ruw, 13,22))
+
+    colnames(datum) <- c("datum")
+    time <- extract(date, c("ruw"), into = c("hours", "minutes"),
+                    "([[:digit:]]+):([[:digit:]]+)")
+    time$time <- paste(time$hours,time$minutes, sep = ":")
+    uur <- time[,3]
+
+    gegevens <- cbind(datum,uur,coords[,1:2])
+    gegevens$x <- as.numeric(gegevens$x)
+    gegevens$y <- as.numeric(gegevens$y)
+    return(gegevens)
 }
 
 
