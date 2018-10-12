734394dcc6a4d203a91970cdc83d66c5bca353a4
damianooldoni <damiano.oldoni@inbo.be>
2018-10-12 23:06:35 +0200

    Improve documentation

--- R/csv_to_sqlite.R
+++ R/csv_to_sqlite.R
@@ -1,12 +1,16 @@
-#' Save a single CSV-table into a single table sqlite database
+#' Save a delimited text table into a single table sqlite database
 #'
+#' The table can be a comma separated (csv) or a tab separated (tsv) or any
+#' other delimited text file. The file is read in chunks. Each chunk is copied
+#' in the same sqlite table database before the next chunk is loaded into
+#' memory.
 #' @param csv_file name of the text file to convert
 #' @param sqlite_file name of the newly created sqlite file
 #' @param table_name name of the table to store the data table in the sqlite
-#'      dbase
+#'   dbase
 #' @param delim text file delimiter (default ",")
 #' @param pre_process_size the number of lines to check the data types of the
-#'      individual columns (default 1000)
+#'   individual columns (default 1000)
 #' @param chunk_size the number of lines to read for each chunk (default 50000)
 #' @param quote same as in \code{readr::read_delim} (default "\"")
 #' @param escape_backslash same as in \code{readr::read_delim} (default FALSE)
@@ -19,6 +23,7 @@
 #' @param trim_ws same as in \code{readr::read_delim} (default FALSE)
 #' @param skip same as in \code{readr::read_delim} (default 0)
 #' @param escape_double same as in \code{readr::read_delim} (default FALSE)
+#' @param callback A callback function to call on each chunk.
 #'
 #' @return a SQLite database
 #'
@@ -52,7 +57,8 @@ csv_to_sqlite <- function(csv_file, sqlite_file, table_name, delim = ",",
     datetime_cols <- df %>%
         select_if(is.POSIXt) %>%
         colnames()
-    # write this first batch of lines to SQLITE table, converting dates to string representation
+    # write the first batch of lines to SQLITE table, converting dates to string
+    # representation
     df <- df %>%
       mutate_at(.vars = date_cols, .funs = as.character.Date) %>%
       mutate_at(.vars = datetime_cols, .funs = as.character.POSIXt)
