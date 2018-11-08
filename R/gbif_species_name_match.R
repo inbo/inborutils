00e2cfd8d86cf733ec43bb61681b5a35692228fd
stijnvanhoey <stijnvanhoey@gmail.com>
2018-11-08 14:46:15 +0100

    Fix error on gbif name matching

--- R/gbif_species_name_match.R
+++ R/gbif_species_name_match.R
@@ -51,15 +51,12 @@ gbif_species_name_match <- function(df, name_col,
     gbif_terms <- match.arg(gbif_terms, API_terms, several.ok = TRUE)
 
     # matching the GBiF matching information to the sample_data
-    df %>% rowwise() %>%
-        do_(interp(~ as.data.frame(name_backbone(name = .$x), 
-                                   stringsAsFactors = FALSE),
-                   x = as.name(name_col))) %>%
-        select(gbif_terms) %>%
-        bind_cols(df)
-    # (remark I use here Standard evaluation (SE) with do_,
-    # see also:
-    # https://cran.r-project.org/web/packages/dplyr/vignettes/programming.html and
-    # https://stackoverflow.com/questions/26739054/using-variable-column-names-in-dplyr-do)
-
+    df %>%
+        mutate(
+            scientific_name = df[[name_col]],
+            gbif = map(scientific_name, name_backbone) %>%
+                map(as.tibble)
+        ) %>%
+        unnest() %>%
+        select(c(colnames(df), gbif_terms))
 }
