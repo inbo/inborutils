db55afa6a9c9a5db137d955e4d0991c8b03cc08f
AnemoneDeroy <49189843+AnemoneDeroy@users.noreply.github.com>
2019-04-02 13:38:54 +0200

    Create aquachem-factresults.R

---
+++ R/aquachem-factresults.R
@@ -0,0 +1,137 @@
+#' Get data from FactResult
+#'
+#' Get all or a subset of FactResult data. Filter options are the location of 
+#' the fieldsampling and date.
+#' 
+#'
+#' @param my_connection A valid connection to ETN database.
+#' @param code_name (string) One or more network projects.
+#' @param sampling_date (string) One or more animal projects.
+#' @param reportable (string) One or more scientific names.
+#'
+#' @return A tibble (tidyverse data.frame).
+#'
+#' @export
+#'
+#' @importFrom glue glue_sql
+#' @importFrom DBI dbGetQuery
+#' @importFrom dplyr pull %>%
+#' @importFrom tibble as_tibble
+#'
+#' @examples
+#' \dontrun{
+#' con <- connect_to_etn(your_username, your_password)
+#'
+#'# get all results where code = AN_BLA_001 , date = 20140630 , reportable = 1
+#'variabelen <- fact_result(my_connection, "AN_BLA_001" , "20140630", "1")
+#'head(variabelen) %>% knitr::kable()
+#'
+#'
+#'install.packages(c("DBI", "glue", "tidyverse"))
+
+library(DBI)
+library(glue)
+library(tidyverse)
+
+my_connection <- DBI::dbConnect(odbc::odbc(), 
+                                .connection_string = "Driver=SQL Server;Server=inbo-sql08-prd.inbo.be,1433;Database=M0003_00_Aquachem;Trusted_Connection=Yes;")
+print(my_connection)
+
+rel_taxa <- dbReadTable(my_connection, "FactResult")
+head(rel_taxa) %>% knitr::kable()
+
+subset_meting <- dbGetQuery(my_connection, 
+                            "SELECT  p.Project
+                            , p.ContractNumber
+                            , w.CODE
+                            , w.Gemeente
+                            , w.SBZ
+                            , w.meetnet
+                            , w.Abiotiek_j
+                            , w.Biotiek_ja
+                            , w.HabtypeSel
+                            , w.HabtypeVel
+                            , w.INSIDE_X
+                            , w.INSIDE_Y
+                            , r.AnalysisDateKey
+                            , r.AnalysisDate
+                            , a.SAPcode
+                            , r.Component
+                            , r.ResultNumeric
+                            , r.ResultFormatted
+                            , r.ResultFormattedNumeric
+                            , r.IsBelowLOQ
+                            , r.IsAboveLOQ
+                            , u.Unit
+                            , s.FieldSampleID  
+                            , s.FieldObserver-- navragen Jo waarom zoveel legen records
+                            , s.FieldSampleRemark
+                            , s.FieldSamplingDateKey
+                            , s.FieldSamplingDate
+                            FROM dbo.FactResult r
+                            INNER JOIN [dbo].[DimSample] s ON s.SampleKey = r.SampleKey
+                            INNER JOIN dbo.DimWaterhabitat w ON w.WaterhabitatKey = r.WaterhabitatKey
+                            INNER JOIN dbo.DimUnit u ON u.UnitKey = r.UnitKey
+                            INNER JOIN dbo.DimProject p ON p.ProjectKey = r.ProjectKey
+                            INNER JOIN dbo.DimAnalysis a ON a.AnalysisKey = r.AnalysisKey
+                            WHERE 1=1
+                            AND s.WaterhabitatKey != -1
+                            AND w.code = 'AN_BLA_001'
+                            AND FieldSamplingDateKey = 20140630
+                            --and r.Component LIKE '%ph%'
+                            AND r.IsReportable = 1")
+
+
+head(subset_meting) %>% knitr::kable()
+
+fact_result <- function(dbase_connection, code_name, sampling_date, reportable) {
+  dbGetQuery(dbase_connection, glue_sql(
+    "SELECT  p.Project
+    , p.ContractNumber
+    , w.CODE
+    , w.Gemeente
+    , w.SBZ
+    , w.meetnet
+    , w.Abiotiek_j
+    , w.Biotiek_ja
+    , w.HabtypeSel
+    , w.HabtypeVel
+    , w.INSIDE_X
+    , w.INSIDE_Y
+    , r.AnalysisDateKey
+    , r.AnalysisDate
+    , a.SAPcode
+    , r.Component
+    , r.ResultNumeric
+    , r.ResultFormatted
+    , r.ResultFormattedNumeric
+    , r.IsBelowLOQ
+    , r.IsAboveLOQ
+    , u.Unit
+    , s.FieldSampleID  
+    , s.FieldObserver-- navragen Jo waarom zoveel legen records
+    , s.FieldSampleRemark
+    , s.FieldSamplingDateKey
+    , s.FieldSamplingDate
+    FROM dbo.FactResult r
+    INNER JOIN [dbo].[DimSample] s ON s.SampleKey = r.SampleKey
+    INNER JOIN dbo.DimWaterhabitat w ON w.WaterhabitatKey = r.WaterhabitatKey
+    INNER JOIN dbo.DimUnit u ON u.UnitKey = r.UnitKey
+    INNER JOIN dbo.DimProject p ON p.ProjectKey = r.ProjectKey
+    INNER JOIN dbo.DimAnalysis a ON a.AnalysisKey = r.AnalysisKey
+    WHERE 1=1
+    AND s.WaterhabitatKey != -1
+    AND w.code = {code_name}
+    AND FieldSamplingDateKey = {sampling_date}
+    --and r.Component LIKE '%ph%'
+    AND r.IsReportable = {reportable}",
+    code_name = code_name,
+    sampling_date = sampling_date,
+    reportable = reportable,
+    .con = dbase_connection))
+}
+
+
+variabelen <- fact_result(my_connection, "AN_BLA_001" , "20140630", "1")
+
+head(variabelen) %>% knitr::kable()
