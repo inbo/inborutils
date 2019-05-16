48a781888587136df7ab74274e26c2e3d2c7ab02
stijnvanhoey <stijnvanhoey@gmail.com>
2019-05-16 15:36:41 +0200

    Fix errors CMD CHECK

--- R/connect_inbo_dbase.R
+++ R/connect_inbo_dbase.R
@@ -13,11 +13,13 @@
 #'
 #' @export
 #'
-#' @importFrom glue glue_sql
 #' @importFrom DBI dbGetQuery dbConnect dbListTables
+#' @importFrom odbc odbc
 #'
 #' @examples
+#' \dontrun{
 #' connection <- connect_inbo_dbase("D0021_00_userFlora")
+#' }
 connect_inbo_dbase <- function(database_name, sql_driver = "SQL Server") {
 
     # datawarehouse databases (sql08) start with an M, S or W; most
@@ -29,7 +31,7 @@ connect_inbo_dbase <- function(database_name, sql_driver = "SQL Server") {
     }
 
     # connect to database
-    conn <- dbConnect(odbc::odbc(),
+    conn <- dbConnect(odbc(),
                       driver = sql_driver,
                       server = server,
                       port = 1433,
@@ -41,18 +43,9 @@ connect_inbo_dbase <- function(database_name, sql_driver = "SQL Server") {
     code_call <- paste(c("library(inborutils)",
                          paste("con <-", gsub(", ", ",\n\t", code_call))),
                        collapse = "\n")
-    on_connection_opened(conn, code_call)
+    on_connection_opened(conn, code_call, server)
 
     return(conn)
-}
-
-# use the existing odbc package methods for the Rstudio Viewer
-on_connection_closed <- function(conn) {
-    odbc:::on_connection_closed(conn)
-}
-
-on_connection_updated <- function(connection, hint) {
-    odbc:::on_connection_updated(connection, hint)
 }
 
 #' Rstudio Viewer integration
@@ -60,11 +53,13 @@ on_connection_updated <- function(connection, hint) {
 #' See https://stackoverflow.com/questions/48936851/calling-odbc-connection-within-function-does-not-display-in-rstudio-connection and https://rstudio.github.io/rstudio-extensions/connections-contract.html#persistence
 #' @param connection odbc connection
 #' @param code dbase connection code
+#' @param server INBO database server name
 #'
 #' @importFrom odbc odbcListObjectTypes odbcListObjects odbcListColumns
 #' odbcPreviewObject odbcConnectionActions
 #' @importFrom DBI dbDisconnect
-on_connection_opened <- function(connection, code) {
+#'
+on_connection_opened <- function(connection, code, server) {
     # make sure we have an observer
     observer <- getOption("connectionObserver")
     if (is.null(observer))
@@ -74,10 +69,17 @@ on_connection_opened <- function(connection, code) {
     display_name <- paste("INBO Databases -", connection@info$dbname)
     server_name <- connection@info$servername
 
+    # provide information no DWH or database
+    if (grepl("08", server)) {
+        type <- "INBO DWH Server"
+    } else if (grepl("07", server)) {
+        type <- "INBO PRD Server"
+    }
+
     # let observer know that connection has opened
     observer$connectionOpened(
         # connection type
-        type = connection@info$dbms.name,
+        type = type,
 
         # name displayed in connection pane
         displayName = display_name,
@@ -97,7 +99,7 @@ on_connection_opened <- function(connection, code) {
             dbDisconnect(connection)
         },
 
-        listObjectTypes = function () {
+        listObjectTypes = function() {
             odbcListObjectTypes(connection)
         },
 
