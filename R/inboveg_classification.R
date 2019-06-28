e113a66a391b72c071e0e5d6d62c4d85b3b6654c
hansvancalster <hans.vancalster@inbo.be>
2019-06-28 11:09:26 +0200

    Whitespace issue?

--- R/inboveg_classification.R
+++ R/inboveg_classification.R
@@ -1,17 +1,17 @@
 #' @title Query Classification information from INBOVEG
 #'
-#' @description This function queries the INBOVEG database for information 
-#' on the field classification (N2000 or BWK-code) of the relevé for one 
-#' or more survey(s) by the name of the survey. See the examples 
+#' @description This function queries the INBOVEG database for information
+#' on the field classification (N2000 or BWK-code) of the relevé for one
+#' or more survey(s) by the name of the survey. See the examples
 #' for how to get information for all surveys.
 #'
-#' @param survey_name A character vector giving the names of the surveys for 
-#' which you want to extract Classification information. If missing, all 
+#' @param survey_name A character vector giving the names of the surveys for
+#' which you want to extract Classification information. If missing, all
 #' surveys are returned.
-#' @param classif A character vector giving the Classification code of the 
-#' vegetation type for which #' you want to extract information. If missing, 
+#' @param classif A character vector giving the Classification code of the
+#' vegetation type for which #' you want to extract information. If missing,
 #' all classifications are returned.
-#' @param connection dbconnection with the database 'Cydonia' 
+#' @param connection dbconnection with the database 'Cydonia'
 #' on the inbo-sql07-prd server
 #' @param collect If FALSE (the default), a remote tbl object is returned. This
 #' is like a reference to the result of the query but the full result of the
@@ -20,22 +20,22 @@
 #' environment.
 #'
 #' @return A remote tbl object (collect = FALSE) or a tibble dataframe (collect
-#' = TRUE) with variables Id, SurveyName, Classification-code, BWK or 
-#' N2000-list, LocalClassification, Description of the Habitattype, Cover-code, 
+#' = TRUE) with variables Id, SurveyName, Classification-code, BWK or
+#' N2000-list, LocalClassification, Description of the Habitattype, Cover-code,
 #' Cover in percentage.
 #'
 #' @importFrom glue glue_sql
 #' @importFrom DBI dbGetQuery
 #' @importFrom assertthat assert_that
 #' @importFrom dplyr collect tbl sql
-#' 
+#'
 #' @export
-#' 
+#'
 #' @examples
 #' \dontrun{
 #' con <- connect_inbo_dbase("D0010_00_Cydonia")
 #' # get a specific classification from a survey and collect the data
-#' classif_info <- inboveg_classification(con, 
+#' classif_info <- inboveg_classification(con,
 #' survey_name = "MILKLIM_Heischraal2012", classif = "4010", collect = TRUE)
 #' # get all surveys, all classifications,  don't collect the data
 #' allecodes <- inboveg_classification(con)
@@ -43,26 +43,26 @@
 #' rm(con)
 #' }
 
-inboveg_classification <- function(connection, 
-                                   survey_name, 
-                                   classif, 
+inboveg_classification <- function(connection,
+                                   survey_name,
+                                   classif,
                                    collect = FALSE) {
-  
+
   assert_that(inherits(connection, what = "Microsoft SQL Server"),
               msg = "Not a connection object to database.")
-  
+
   if (missing(survey_name)) {
     survey_name <- "%"
   } else {
     assert_that(is.character(survey_name))
   }
-  
+
   if (missing(classif)) {
     classif <- "%"
   } else {
     assert_that(is.character(classif))
-  }  
-  
+  }
+
   sql_statement <- glue_sql(
     "Select ivR.RecordingGivid
     , ivS.Name
@@ -79,24 +79,24 @@ inboveg_classification <- function(connection,
     LEFT JOIN [dbo].[ivRLResources] ivRLRes_Class on ivRLRes_Class.ResourceGIVID = ivRLClas.ClassifResource
     LEFT JOIN [syno].[Futon_dbo_ftActionGroupList] ftAGL_Class on ftAGL_Class.ActionGroup = ivRLRes_Class.ActionGroup collate Latin1_General_CI_AI
     AND ftAGL_Class.ListName = ivRLRes_Class.ListName collate Latin1_General_CI_AI
-    LEFT JOIN [syno].[Futon_dbo_ftBWKValues] ftBWK on ftBWK.Code = ivRLClas.Classif collate Latin1_General_CI_AI 
-    AND ftBWK.ListGIVID = ftAGL_Class.ListGIVID 
-    LEFT JOIN [syno].[Futon_dbo_ftN2kValues] ftN2K on ftN2K.Code = ivRLClas.Classif collate Latin1_General_CI_AI 
-    AND ftN2K.ListGIVID = ftAGL_Class.ListGIVID 
+    LEFT JOIN [syno].[Futon_dbo_ftBWKValues] ftBWK on ftBWK.Code = ivRLClas.Classif collate Latin1_General_CI_AI
+    AND ftBWK.ListGIVID = ftAGL_Class.ListGIVID
+    LEFT JOIN [syno].[Futon_dbo_ftN2kValues] ftN2K on ftN2K.Code = ivRLClas.Classif collate Latin1_General_CI_AI
+    AND ftN2K.ListGIVID = ftAGL_Class.ListGIVID
     LEFT JOIN [dbo].[ivRLResources] ivRLR_C on ivRLR_C.ResourceGIVID = ivRLClas.CoverResource
     LEFT JOIN [syno].[Futon_dbo_ftActionGroupList] ftAGL_C on ftAGL_C.ActionGroup = ivRLR_C.ActionGroup collate Latin1_General_CI_AI
     AND ftAGL_C.ListName = ivRLR_C.ListName collate Latin1_General_CI_AI
     LEFT JOIN [syno].[Futon_dbo_ftCoverValues] ftC on ftC.Code = ivRLClas.Cover collate Latin1_General_CI_AI
-    AND ftAGL_C.ListGIVID = ftC.ListGIVID 
-    WHERE ivRLClas.Classif is not NULL 
+    AND ftAGL_C.ListGIVID = ftC.ListGIVID
+    WHERE ivRLClas.Classif is not NULL
     AND ivS.Name LIKE {survey_name}
     AND ivRLClas.Classif LIKE {classif}",
     survey_name = survey_name,
     classif = classif,
     .con = connection)
-  
+
   query_result <- tbl(connection, sql(sql_statement))
-  
+
   if (!isTRUE(collect)) {
     return(query_result)
   } else {
