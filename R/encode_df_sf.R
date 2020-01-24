17ec1e5d4fb511759abcb50b26285e3fb77ce536
florisvdh <floris.vanderhaeghe@inbo.be>
2020-01-24 17:02:58 +0100

    Move watina's convertdf_enc() to inborutils

---
+++ R/encode_df_sf.R
@@ -0,0 +1,46 @@
+#' Convert encoding of character and factor variables in a dataframe
+#'
+#' @param x A dataframe.
+#' @param to Encoding string to convert to.
+#' All \code{R} platforms support \code{""} (for the encoding of the current
+#' locale), \code{"latin1"} and \code{"UTF-8"}.
+#' See \code{\link[base]{iconv}} for more information.
+#'
+#' @return
+#' The original dataframe, with character variables (and levels of
+#' (character) factor variables) converted to the specified encoding.
+#'
+#' @export
+#' @importFrom dplyr
+#' %>%
+#' mutate_if
+#' @importFrom assertthat
+#' assert_that
+#' is.string
+convertdf_enc <- function(x,
+                          to = "UTF-8") {
+
+    assert_that(inherits(x, "data.frame"))
+    assert_that(is.string(to))
+
+    is_chfact <- function(vec) {
+        if (is.factor(vec)) {
+            is.character(levels(vec))
+        } else FALSE
+    }
+
+    conv_levels <- function(fact, to) {
+        levels(fact) <- iconv(levels(fact),
+                              to = to)
+        return(fact)
+    }
+
+    x %>%
+        mutate_if(is.character,
+                  iconv,
+                  to = to) %>%
+        mutate_if(is_chfact,
+                  conv_levels,
+                  to = to)
+
+}
