6e4544099a4114047003d915f6b692298d17fe0a
damianooldoni <damiano.oldoni@inbo.be>
2020-05-19 17:20:37 +0200

    Remove deprecation warning for CRS.from_proj4()

--- tests/testthat/test-reproject_coordinates.R
+++ tests/testthat/test-reproject_coordinates.R
@@ -10,9 +10,11 @@ library(readr)
 #   stringsAsFactors = FALSE
 # )
 data_pts  <- read_tsv("./data_test_project_coordinate/data_pts_input.tsv")
-sp_crs1 <- CRS("+init=epsg:4269")
+# epsg 4269
+sp_crs1 <- CRS("+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs")
 sf_crs1 <- st_crs(4269)
-sp_crs2 <- CRS("+init=epsg:3857")
+# epsg 3857
+sp_crs2 <- CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")
 sf_crs2 <- st_crs(3857)
 
 data_out_sp_sp <- reproject_coordinates(data_pts, col_long = lon, col_lat = lat,
