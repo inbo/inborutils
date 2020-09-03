e7e0e862724d359df560e434fca137cc453ab55b
damianooldoni <damiano.oldoni@inbo.be>
2020-09-03 18:19:17 +0200

    add normalizePath again with arg mustWork = FALSE
    Co-Authored-By: Thierry Onkelinx <ThierryO@users.noreply.github.com>

--- R/setup_codingclub_session.R
+++ R/setup_codingclub_session.R
@@ -68,7 +68,9 @@ setup_codingclub_session <- function(
     session_date
   )
   src_target_dir <- file.path(root_dir, src_rel_path, session_date)
+  src_target_dir <- normalizePath(src_target_dir, mustWork = FALSE)
   data_target_dir <- file.path(root_dir, data_rel_path, session_date)
+  data_target_dir <- normalizePath(data_target_dir, mustWork = FALSE)
   message(sprintf(
     "R scripts in %s will be downloaded in folder: %s",
     github_src_link, src_target_dir
