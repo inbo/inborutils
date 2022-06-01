c5556a5a5d1f77c5447ee6729f374946390d64a6
hansvancalster <hans.vancalster@inbo.be>
2022-06-01 18:01:44 +0200

    capitalize epsg

--- tests/testthat/test-reproject_coordinates.R
+++ tests/testthat/test-reproject_coordinates.R
@@ -9,13 +9,13 @@ library(readr)
 #   stringsAsFactors = FALSE
 # )
 data_pts  <- read_tsv("./data_test_project_coordinate/data_pts_input.tsv")
-# epsg 4269
+# EPSG 4269
 if (requireNamespace("sp")) {
   sp_crs1 <- sp::CRS("+proj=longlat +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +no_defs")
   sp_crs2 <- sp::CRS("+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs")
 }
 sf_crs1 <- st_crs(4269)
-# epsg 3857
+# EPSG 3857
 sf_crs2 <- st_crs(3857)
 
 data_out_sp_sp <- reproject_coordinates(data_pts, col_long = lon, col_lat = lat,
